4f8d4643e127bde191e51f2142f1ccaee2e2d325
klh5 <k.awtycarroll@gmail.com>
2023-06-14 15:08:16 +0000

    Implement conversion of Action to one-hot-encoding

--- src/model/action.rs
+++ src/model/action.rs
@@ -1,8 +1,22 @@
 use serde::{Deserialize, Serialize};
+use tch::Tensor;
+
+//   one_hot(3) -> tensor of shape [3, 3]
+
 
 #[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
 pub enum Action {
     ToFood,
     ToWater,
     Stationary,
+}
+
+impl Action {
+    pub fn encode(&self) -> Tensor {
+        match self {
+            Action::ToFood => Tensor::from_slice(&[1, 0, 0]).internal_cast_float(true),
+            Action::ToWater => Tensor::from_slice(&[0, 1, 0]).internal_cast_float(true),
+            Action::Stationary => Tensor::from_slice(&[0, 0, 1]).internal_cast_float(true),
+        }  
+    } 
 }
