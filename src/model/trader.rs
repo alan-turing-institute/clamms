3c03d129352e1149f0e1184326c8027f0d47c5f0
Tim Hobson <thobson88@gmail.com>
2023-06-14 18:13:06 +0100

    Add Offer tuple struct

--- src/model/trader.rs
+++ src/model/trader.rs
@@ -19,14 +19,38 @@ impl Trader {
     }
 }
 
-// pub struct Offer<Vec<Resource>> {
+// pub type Offer = (i8, i8);
 
-// }
+pub struct Offer(i8, i8);
 
-// pub trait Trade {
-//     fn offer(&self, resource: Resource) -> u32;
-//     fn is_viable(&self, resource: Resource) -> bool;
-// }
+impl Offer {
+
+    fn new(lots_food: i8, lots_water: i8) -> Self {
+        if lots_food > 0 && lots_water > 0 {
+            panic!();
+        }
+        if lots_food < 0 && lots_water < 0 {
+            panic!();
+        }
+        Offer(lots_food, lots_water)
+    }
+
+    fn delta(&self, resource: Resource) -> i8 {
+        match resource {
+            Resource::Food => self.0,
+            Resource::Water => self.1
+        }
+    }
+
+    /// Determines whether this offer is matched by another offer.
+    fn matched(&self, offer: Offer) -> bool {
+        std::cmp::max(self.0 + offer.0, self.1 + offer.1) <= 0
+    }
+}
+
+pub trait Trade {
+    fn offer(&self) -> Offer;
+}
 
 
 // impl Trade for Trader {
@@ -76,5 +100,28 @@ impl Hash for Trader {
         H: Hasher,
     {
         self.id().hash(state);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::{model::{forager::Forager, trader::Trader}, config::core_config};
+
+    #[test]
+    fn test_matched() {
+
+        // This is an offer of *at most* 2 lots of food for *at least* 3 lots of water.
+        let offer = Offer::new(-2, 3);
+
+        assert!(offer.matched(Offer::new(2, -3)));
+        assert!(offer.matched(Offer::new(2, -5)));
+        assert!(offer.matched(Offer::new(1, -3)));
+        assert!(offer.matched(Offer::new(1, -4)));
+        assert!(offer.matched(Offer::new(0, -3)));
+        
+        assert!(!offer.matched(Offer::new(3, -3)));
+        assert!(!offer.matched(Offer::new(2, -2)));
+        assert!(!offer.matched(Offer::new(2, -1)));
     }
 }
