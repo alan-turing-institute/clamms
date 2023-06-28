2a46610d0f31d19d40e05ab9e1de76e688b48485
Sam Greenbury <sgreenbury@turing.ac.uk>
2023-06-28 17:04:02 +0100

    Add construct trait, corner and river structs

--- src/model/board_factory.rs
+++ src/model/board_factory.rs
@@ -55,5 +55,108 @@ pub fn example_board(dim: (u16, u16)) -> BTreeMap<Resource, Vec<ClammsInt2D>> {
     map
 }
 
+trait Construct {
+    fn generate(&self) -> BTreeMap<Resource, Vec<ClammsInt2D>>;
+}
+
+struct CornerBoard {
+    water_loc: ClammsInt2D,
+    food_loc: ClammsInt2D,
+    water_size: i32,
+    food_size: i32,
+    dim: (usize, usize),
+}
+
+impl Construct for CornerBoard {
+    fn generate(&self) -> BTreeMap<Resource, Vec<ClammsInt2D>> {
+        let mut map = BTreeMap::new();
+        map.insert(Resource::Food, vec![]);
+        map.insert(Resource::Water, vec![]);
+        // Set water locations
+        assert!((self.water_loc.x + self.water_size) < self.dim.0.try_into().unwrap());
+        assert!((self.water_loc.y + self.water_size) < self.dim.1.try_into().unwrap());
+        for i in self.water_loc.x..self.water_size {
+            for j in self.water_loc.y..(self.water_loc.y + self.water_size) {
+                let v = map.get_mut(&Resource::Water).unwrap();
+                v.push(ClammsInt2D { x: i, y: j });
+            }
+        }
+        // Set food locations
+        assert!((self.food_loc.x + self.food_size) < self.dim.0.try_into().unwrap());
+        assert!((self.food_loc.y + self.food_size) < self.dim.1.try_into().unwrap());
+        for i in self.food_loc.x..(self.food_loc.x + self.food_size) {
+            for j in self.food_loc.y..(self.food_loc.y + self.food_size) {
+                let v = map.get_mut(&Resource::Food).unwrap();
+                v.push(ClammsInt2D { x: i, y: j });
+            }
+        }
+        map
+    }
+}
+
+struct RiverBoard {
+    _water_loc: ClammsInt2D,
+    food_loc: ClammsInt2D,
+    water_size: i32,
+    food_size: i32,
+    dim: (usize, usize),
+}
+
+impl Construct for RiverBoard {
+    fn generate(&self) -> BTreeMap<Resource, Vec<ClammsInt2D>> {
+        let mut map = BTreeMap::new();
+        map.insert(Resource::Food, vec![]);
+        map.insert(Resource::Water, vec![]);
+        // Set food locations
+        assert!((self.food_loc.x + self.food_size) < self.dim.0.try_into().unwrap());
+        assert!((self.food_loc.y + self.food_size) < self.dim.1.try_into().unwrap());
+        for i in self.food_loc.x..self.food_size {
+            for j in self.food_loc.y..(self.food_loc.y + self.food_size) {
+                let v = map.get_mut(&Resource::Food).unwrap();
+                v.push(ClammsInt2D { x: i, y: j });
+            }
+        }
+        // Set water locations
+        let mut rng = StdRng::seed_from_u64(1);
+        let mut river_width = 0i32;
+        for j in 1..self.dim.1 {
+            river_width += rng.gen_range(-(self.water_size / 2)..=(self.water_size / 2));
+            for i in (self.dim.0 / 2 - usize::try_from(self.water_size).unwrap())
+                ..=(self.dim.0 / 2 + usize::try_from(self.water_size).unwrap())
+            {
+                let v = map.get_mut(&Resource::Water).unwrap();
+                v.push(ClammsInt2D {
+                    x: (i32::try_from(i).unwrap() - i32::try_from(self.dim.0 / 2).unwrap()
+                        + river_width),
+                    y: i32::try_from(j).unwrap(),
+                });
+            }
+        }
+        map
+    }
+}
+
 #[cfg(test)]
-mod tests {}
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_corner_board() {
+        let dim = (10usize, 10usize);
+        let corner_board = CornerBoard {
+            water_loc: ClammsInt2D { x: 0, y: 0 },
+            food_loc: ClammsInt2D {
+                x: i32::try_from(dim.0).unwrap() - 3,
+                y: i32::try_from(dim.1).unwrap() - 3,
+            },
+            water_size: 2,
+            food_size: 2,
+            dim,
+        };
+        println!("{:?}", corner_board.generate());
+    }
+    #[test]
+    fn test_river_board() {
+        todo!()
+    }
+}
